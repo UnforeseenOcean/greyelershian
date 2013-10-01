0ddc95db8c3635cb3c9e1f1d803e6f558b9c222c
rektide <rektide@voodoowarez.com>
2013-10-01 17:22:00 -0400

    Initial Greyelershian.

---
+++ greyelershian.js
@@ -0,0 +1,35 @@
+function extractProperty(lookups,property){
+	lookups.forEach(_makeExtractProperty(property)
+	return lookups
+}
+function _makeExtractProperty(prop){
+	function _extractProperty(val,key,arr){
+		arr[key]= val?val[property]:undefined
+	}
+}
+function validateHas(lookups){
+	lookups.forEach(_validate)
+}
+function _validate(val,key,arr){
+	if(!val)
+		console.error("Missing element for key: ",key)
+}
+
+// VALIDATE AND EXTRACT DATA
+//var els= lookupSelectors(concerns)
+//validateHas(els)
+//var colors= extractProperty("background-color")
+// OR DONT
+var colors= {label: "rgb(203, 68, 55)"
+  icon: "rgb(102, 102, 102)"}
+
+var sheet= "\
+.gb_jb { background-color: "+ colors.icon +"; transition: background-color 1.2s; }; \ 
+.gb_jb:hover { background-color: "+ colors.label +"; }; \
+";
+
+// DOMfunky https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet.insertRule
+var styleSheet= document.createElement("style")
+document.getElementsByTagName('head')[0].appendChild(styleSheet)
+
+})(
